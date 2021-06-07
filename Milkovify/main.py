7e89c98fbf411beb476e047cd89940709c086fd4
Nekurone <Joshmcd100@gmail.com>
2021-06-08 00:27:41 +0100

    Initial Clone of Bluebell.
    Upload of renamed Bluebell Core.

---
+++ Milkovify/main.py
@@ -0,0 +1,16 @@
+from bot import Client
+from Core.config import logger, TOKEN
+
+COGS = [
+    "owner",
+    "stats",
+    "testing",
+    ]
+
+
+def main():
+    client = Client(COGS)
+    logger.GENERAL('Booting Bluebell Core with {0} Cogs.'.format(len(COGS)))
+    client.run(TOKEN)
+
+main()
