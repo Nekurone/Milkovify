74308a91c42d55bbad76507229c9fa7d1df68274
Nekurone <zx627924@ou.ac.uk>
2021-06-09 03:44:12 +0100

    Update stats.py
    Fixed issue regarding relative imports (again)

--- Milkovify/Cogs/stats.py
+++ Milkovify/Cogs/stats.py
@@ -6,9 +6,9 @@ import psutil
 import discord
 from discord.ext import commands
 
-from ..Core.config import logger, github
-from ..Core import checks
-from ..Core.Utils.chat_formatter import humanize_timedelta
+from Core.config import logger, github
+from Core import checks
+from Core.Utils.chat_formatter import humanize_timedelta
 
 
 def setup(client):
